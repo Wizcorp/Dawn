3ce5c66e93afe6caeb13f826f8370add34126ffc
stelcheck <mtrudel@wizcorp.jp>
2017-02-28 18:23:05 +0900

    removed local-allinone tpl, working local tpl
    Working version of local template. Still needs a bit of polish, but
    currently works as intended.

--- templates/local/create.sh
+++ templates/local/create.sh
@@ -1,1 +1,14 @@
-echo "hello!"
+# Notify user
+echo ""
+echo "This container will now exit; please run the following locally:"
+echo ""
+echo "  cd ./dawn/${DAWN_ENVIRONMENT}; vagrant up"
+echo ""
+echo "Then run 'dawn ${DAWN_ENVIRONMENT}' once again to start provisioning"
+echo ""
+
+# Self destruct
+rm create.sh
+
+# Exit
+exit 0
---
+++ templates/local/run.sh
@@ -0,0 +1,26 @@
+if
+    [ ! -f ./inventory ]
+then
+    echo "You must first run Vagrant locally! "
+    exit 1
+fi
+
+ln -s ${DAWN_ENVIRONMENT_FILES_PATH}/inventory /etc/ansible/hosts
+
+cat << EOF
+* Docker Swarm:
+  - Leader-1: 10.0.0.50:2376
+  - Worker-1: 10.0.0.100:2376
+  - Worker-2: 10.0.0.101:2376
+  - Balancer: 10.0.0.200:2376
+* Monitoring:
+  - Kibana: http://10.0.0.20:5601/
+  - ElasticSearch: http://10.0.0.20:9200/
+  - Grafana: http://10.0.0.20:3000/ (admin:admin)
+* Service Discovery:
+  - Consul: http://10.0.0.20:8500/ui/
+  - Consul DNS: 10.0.0.20:8600
+  - DNSMasq DNS: 10.0.0.20:53
+* Load Balancing:
+  - Traefik: http://10.0.0.200
+EOF
