2a10e9b06c2c6ac4557afa1d1192ec298a2ca63d
stelcheck <mtrudel@wizcorp.jp>
2017-03-01 00:06:36 +0900

    Removed dashboard link, local template run script
    Local template run script now shows a few links to different tools
    and dashboards provided by dawn.

--- scripts/setup/98_motd.sh
+++ scripts/setup/98_motd.sh
@@ -8,6 +8,5 @@ then
   echo ""
   printf "%-10s %s\n" "Project:" "${DAWN_PROJECT_NAME}"
   printf "%-10s %s\n" "Environment:" "${DAWN_ENVIRONMENT}"
-  printf "%-10s %s\n" "Dashboard:" "${DAWN_ENVIRONMENT_DASHBOARD_URL}"
   echo ""
 fi
--- templates/local/run.sh
+++ templates/local/run.sh
@@ -5,22 +5,29 @@ then
     exit 1
 fi
 
+# Link inventory to global hosts file
 ln -s ${DAWN_ENVIRONMENT_FILES_PATH}/inventory /etc/ansible/hosts
 
-cat << EOF
-* Docker Swarm:
-  - Leader-1: 10.0.0.50:2376
-  - Worker-1: 10.0.0.100:2376
-  - Worker-2: 10.0.0.101:2376
-  - Balancer: 10.0.0.200:2376
-* Monitoring:
-  - Kibana: http://10.0.0.20:5601/
-  - ElasticSearch: http://10.0.0.20:9200/
-  - Grafana: http://10.0.0.20:3000/ (admin:admin)
-* Service Discovery:
-  - Consul: http://10.0.0.20:8500/ui/
-  - Consul DNS: 10.0.0.20:8600
-  - DNSMasq DNS: 10.0.0.20:53
-* Load Balancing:
-  - Traefik: http://10.0.0.200
-EOF
+# get hosts out of inventory
+hosts="$(cat inventory | grep ansible_ssh_host | cut -d" " -f1-2 | sed "s/ansible_ssh_host=//g")"
+
+# set DOCKER_HOST
+export DAWN_EDGE="$(echo ${hosts} | grep edge | head -n1 | cut -d " " -f2)"
+export DAWN_CONTROL="$(echo ${hosts} | grep control | head -n1 | cut -d " " -f2)"
+
+# set DOCKER_HOST
+export DOCKER_HOST="http://${DAWN_CONTROL}:2376"
+
+# display additional information
+echo "* Monitoring:"
+printf "%-15s %s\n" "  - Kibana:" "http://${DAWN_CONTROL}:5601/"
+printf "%-15s %s\n" "  - ElasticSearch:" "http://${DAWN_CONTROL}:9200/"
+printf "%-15s %s\n" "  - Grafana:" "http://${DAWN_CONTROL}:3000/"
+
+echo "* Service Discovery:"
+printf "%-15s %s\n" "  - Consul:" "http://${DAWN_CONTROL}:8500/ui/"
+printf "%-15s %s\n" "  - Consul DNS:" "${DAWN_CONTROL}:8600"
+printf "%-15s %s\n" "  - DNSMasq DNS:" "${DAWN_CONTROL}:53"
+
+echo "* Load Balancing"
+printf "%-15s %s\n" "  - Traefik:" "http://${DAWN_EDGE}"
