cbe0134f6ad89a154137edd73ad3110b8bf81b4a
Marc Trudel <mtrudel@wizcorp.jp>
2017-03-09 18:50:20 +0900

    Variable rename
    NAME was renamed to BINARY_NAME in a separate commit

--- docker-image/scripts/setup/98_motd.sh
+++ docker-image/scripts/setup/98_motd.sh
@@ -3,7 +3,7 @@
 if
   [ "${COMMAND:0:4}" == "bash" ]
 then
-  figlet ${NAME}
+  figlet ${BINARY_NAME}
   printf "%-15s %s\n" "Project:" "${PROJECT_NAME}"
   printf "%-15s %s\n" "Environment:" "${PROJECT_ENVIRONMENT}"
   echo ""
