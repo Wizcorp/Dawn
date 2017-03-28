4c50f7916870decc5d10d692685d20c2b0932d2e
Christophe Robin <crobin@nekoo.com>
2017-03-28 17:51:23 +0900

    Working Vault, Consul and Docker TLS provisioning

--- docker-image/templates/local/run.sh
+++ docker-image/templates/local/run.sh
@@ -15,8 +15,37 @@ hosts="$(cat inventory | grep ansible_ssh_host | cut -d" " -f1-2 | sed "s/ansibl
 export EDGE_NODE="$(echo ${hosts} | grep edge | head -n1 | cut -d " " -f2)"
 export CONTROL_NODE="$(echo ${hosts} | grep control | head -n1 | cut -d " " -f2)"
 
+# set VAULT
+if
+    [ -f /home/dawn/.vault.json ]
+then
+    export VAULT_ADDR="http://${CONTROL_NODE}:8200"
+    export VAULT_TOKEN="$( jq -r '.root_token' /home/dawn/.vault.json )"
+fi
+
 # set DOCKER_HOST
-export DOCKER_HOST="${CONTROL_NODE}:2376"
+export DOCKER_HOST="${CONTROL_NODE}:2375"
+
+if
+    [ ! -z "${VAULT_TOKEN}" ]
+then
+    export DOCKER_HOST="tcp://${CONTROL_NODE}:2376"
+    export DOCKER_TLS_VERIFY=1
+
+    # Get a new certificate set from vault for this connection
+    mkdir /home/dawn/.docker >/dev/null 2>&1 || true
+
+    if
+        vault write --format=json \
+            docker/pki/issue/client.dawn \
+            common_name=local.client.dawn > /home/dawn/.docker/vault.json
+    then
+        jq -r .data.certificate /home/dawn/.docker/vault.json > /home/dawn/.docker/cert.pem
+        jq -r .data.issuing_ca /home/dawn/.docker/vault.json  > /home/dawn/.docker/ca.pem
+        jq -r .data.private_key /home/dawn/.docker/vault.json > /home/dawn/.docker/key.pem
+    fi
+fi
+
 
 # display additional information
 echo "* Monitoring:"
@@ -32,3 +61,11 @@ printf "%-15s %s\n" "  - DNSMasq DNS:" "${CONTROL_NODE}:53"
 echo "* Load Balancing"
 printf "%-15s %s\n" "  - Traefik:" "http://${EDGE_NODE}"
 echo ""
+
+if
+    [ ! -z "${VAULT_TOKEN}" ]
+then
+    echo "* Security"
+    printf "%-15s %s\n" "  - Vault:" "${VAULT_ADDR}"
+    echo ""
+fi
