70408438915d23e2afe642c2db768b69d55d7d2b
Christophe Robin <crobin@nekoo.com>
2017-04-20 15:39:07 +0900

    Allow access to vault through Traefik
    * Make intermediate CAs available on all machines as part of the trust chain
    * Add a traefik consul entry for vault
    * Change the docker image env to point to the edge nodes instead of control

--- docker-image/scripts/setup/40_vault.sh
+++ docker-image/scripts/setup/40_vault.sh
@@ -6,6 +6,16 @@
 
 VAULT_CERT_PATH="${HOME}/certs/vault"
 
+export VAULT_ADDR="https://${CONTROL_NODE}:8200"
+
+# If we can resolve the local domain properly, use that instead (usually local
+# dev envs won't be able to resolve unless the user changes his DNS)
+if
+    nslookup "vault.${LOCAL_DOMAIN}" 1>/dev/null 2>&1
+then
+    export VAULT_ADDR="http://vault.${LOCAL_DOMAIN}"
+fi
+
 # set VAULT environment variables, we first attempt to login using a generic
 # configuration, this configuration is created by the admin for each user and
 # should be installed manually on first setup. If this configuration does not
@@ -20,7 +30,6 @@ then
     VAULT_AUTH_BACKEND="$( jq -r .backend "${HOME}/.vault.conf" )"
     VAULT_AUTH_DATA="$( jq -cM .data "${HOME}/.vault.conf" )"
 
-    export VAULT_ADDR="https://${CONTROL_NODE}:8200"
     export VAULT_TOKEN="$( curl --connect-timeout 3 --cacert "${VAULT_CACERT}" -XPOST -sS "${VAULT_ADDR}/v1/auth/${VAULT_AUTH_BACKEND}/login" -d "${VAULT_AUTH_DATA}" | jq -r .auth.client_token )"
 elif
     [ -f "${HOME}/.vault.ansible.conf" ]
@@ -29,7 +38,6 @@ then
     export VAULT_CLIENT_CERT="${VAULT_CERT_PATH}/client.cert.pem"
     export VAULT_CLIENT_KEY="${VAULT_CERT_PATH}/client.key.pem"
 
-    export VAULT_ADDR="https://${CONTROL_NODE}:8200"
     export VAULT_TOKEN="$( curl --connect-timeout 3 --cacert "${VAULT_CACERT}" -XPOST -sS "${VAULT_ADDR}/v1/auth/approle/login" -d "$( cat ${HOME}/.vault.ansible.conf )" | jq -r .auth.client_token )"
 elif
     [ -f "${HOME}/.vault.root.conf" ]
@@ -38,7 +46,6 @@ then
     export VAULT_CLIENT_CERT="${VAULT_CERT_PATH}/client.cert.pem"
     export VAULT_CLIENT_KEY="${VAULT_CERT_PATH}/client.key.pem"
 
-    export VAULT_ADDR="https://${CONTROL_NODE}:8200"
     export VAULT_TOKEN="$( jq -r '.root_token' /home/dawn/.vault.root.conf )"
 else
     cat <<- EOM
