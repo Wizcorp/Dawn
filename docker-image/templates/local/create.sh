002d17462eb3a7d378276875a20ece286dd86377
Christophe Robin <crobin@nekoo.com>
2017-05-16 18:20:08 +0900

    Add grafana password generation

--- docker-image/templates/local/create.sh
+++ docker-image/templates/local/create.sh
@@ -18,8 +18,17 @@ echo ""
 echo "" >> ansible/group_vars/all
 echo "# LDAP administration password, generated on environment creation, make" >> ansible/group_vars/all
 echo "# sure to save this somewhere safe" >> ansible/group_vars/all
-echo -n "ldap_admin_password: " >> ansible/group_vars/all
 
+echo -n "ldap_admin_password: " >> ansible/group_vars/all
+python -c "import random, string; print(''.join(random.SystemRandom().choice(string.letters + string.digits + string.punctuation) for _ in range(24)));" >> ansible/group_vars/all
+
+# Same thing but for grafana
+echo "" >> ansible/group_vars/all
+echo "# Grafana administration password, it is only used by the ansible playbook" >> ansible/group_vars/all
+echo "# to manage dashboards and configuration. To access grafana please create" >> ansible/group_vars/all
+echo "# a user in LDAP instead" >> ansible/group_vars/all
+
+echo -n "grafana_password: " >> ansible/group_vars/all
 python -c "import random, string; print(''.join(random.SystemRandom().choice(string.letters + string.digits + string.punctuation) for _ in range(24)));" >> ansible/group_vars/all
 
 # Self destruct
