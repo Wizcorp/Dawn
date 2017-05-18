a20dfe360390a4a85309ffc730aa1c41f5a301b1
Christophe Robin <crobin@nekoo.com>
2017-05-18 12:28:46 +0900

    Fix traefik variable collision, domain detection

--- docker-image/scripts/setup/01_env.sh
+++ docker-image/scripts/setup/01_env.sh
@@ -3,11 +3,10 @@ sudo ln -sfT ${PROJECT_ENVIRONMENT_FILES_PATH}/inventory /etc/ansible/hosts
 
 # get hosts out of inventory
 hosts="$(cat inventory | grep ansible_ssh_host | sed -r 's/^([a-z0-9\._-]+).*ansible_ssh_host="([^"]+)".*/\1 \2/gi')"
-hostvars="$(ANSIBLE_STDOUT_CALLBACK=json ansible-playbook /dawn/ansible/dump_facts.yml)"
 
 # fetch domain names
-export LOCAL_DOMAIN="$(echo ${hostvars} | jq -r '[..|.local_domain_name?]|map(select(.))|unique[0]')"
-export LOCAL_DOMAIN_DC="$(echo ${hostvars} | jq -r '[..|.local_domain_dc?]|map(select(.))|unique[0]')"
+export LOCAL_DOMAIN="$( grep '^local_domain_name' ansible/group_vars/all | cut -f 2 -d ' ' )"
+export LOCAL_DOMAIN_DC="$( grep '^local_domain_dc' ansible/group_vars/all | cut -f 2 -d ' ' )"
 
 # set DOCKER_HOST
 export EDGE_NODE="$(echo "${hosts}" | grep edge | head -n1 | cut -d " " -f2)"
