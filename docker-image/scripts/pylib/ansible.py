f58295856d60bb34822eca1be658d6791559374d
Christophe Robin <crobin@nekoo.com>
2018-03-29 16:13:20 +0900

    Downgraded Ansible back to 2.3.x due to compatibility issues

--- docker-image/scripts/pylib/ansible.py
+++ docker-image/scripts/pylib/ansible.py
@@ -1,20 +1,15 @@
 from __future__ import absolute_import
 
-import os
-
-from ansible.errors import (AnsibleFileNotFound, AnsibleParserError,
-                            AnsibleUndefinedVariable)
-from ansible.inventory.manager import InventoryManager
+from ansible.compat.six import string_types
+from ansible.errors import AnsibleFileNotFound, AnsibleParserError, AnsibleUndefinedVariable
+from ansible.inventory import Inventory
 from ansible.module_utils._text import to_bytes
-from ansible.module_utils.six import binary_type, text_type
 from ansible.parsing.dataloader import DataLoader
 from ansible.parsing.vault import is_encrypted
-from ansible.playbook.play import Play
-from ansible.playbook.task import Task
 from ansible.template import Templar
-from ansible.utils.vars import combine_vars
-from ansible.vars.hostvars import HostVars
-from ansible.vars.manager import VariableManager
+from ansible.vars import VariableManager, combine_vars
+
+import os
 
 
 # this is a custom loader that ignores anything that is encrypted with vault
@@ -24,14 +19,13 @@ class CustomLoader(DataLoader):
         Reads the file contents from the given file name, and will decrypt them
         if they are found to be vault-encrypted.
         '''
-        if not file_name or not isinstance(file_name, (binary_type, text_type)):
+        if not file_name or not isinstance(file_name, string_types):
             raise AnsibleParserError("Invalid filename: '%s'" % str(file_name))
 
-        b_file_name = to_bytes(self.path_dwim(file_name))
-        # This is what we really want but have to fix unittests to make it pass
-        # if not os.path.exists(b_file_name) or not os.path.isfile(b_file_name):
+        b_file_name = to_bytes(file_name)
         if not self.path_exists(b_file_name) or not self.is_file(b_file_name):
-            raise AnsibleFileNotFound("Unable to retrieve file contents", file_name=file_name)
+            raise AnsibleFileNotFound("the file_name '%s' does not exist, or "
+                                      "is not readable" % file_name)
 
         show_content = True
         try:
@@ -55,28 +49,35 @@ class AnsibleEnvironment():
     def __init__(self):
         ansible_basedir = os.path.join(
             os.environ.get("PROJECT_ENVIRONMENT_FILES_PATH"), "ansible")
-        inv_file = '/etc/ansible/hosts'
 
         loader = CustomLoader()
         loader.set_basedir(ansible_basedir)
 
-        # load the inventory, set the basic playbook directory
-        self._inventory = InventoryManager(loader=loader, sources=[inv_file])
-        var_manager = VariableManager(loader=loader, inventory=self._inventory)
-        hostvars = HostVars(inventory=self._inventory, variable_manager=var_manager, loader=loader)
-        play = Play.load(dict(hosts=['all']), loader=loader, variable_manager=var_manager)
+        var_manager = VariableManager()
 
-        group = self._inventory.groups['all']
+        # load the inventory, set the basic playbook directory
+        self._inventory = Inventory(
+            loader=loader,
+            variable_manager=var_manager
+        )
+        self._inventory.set_playbook_basedir(ansible_basedir)
+
+        group = self._inventory.get_group("all")
 
         # make sure we load all magic variables on top of the global variables
         self._vars = combine_vars(
-            var_manager.get_vars(
-                play=play,
-                task=Task(),
-                host=group.get_hosts()[0]
-            ),
-            {'env': os.environ}
+            self._inventory.get_group_vars(group, return_results=True),
+            var_manager._get_magic_variables(loader, False, None, None,
+                                             False, False)
         )
+        self._vars['groups'] = self._inventory.get_group_dict()
+        self._vars['env'] = os.environ
+
+        hostvars = {}
+        for host in self._inventory.get_hosts():
+            hostvars[host.name] = host.get_vars()
+
+        self._vars['hostvars'] = hostvars
 
         # create the template renderer
         self._templar = Templar(loader=loader, variables=self._vars)
