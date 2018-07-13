c3609c4c3662cd7f014212f11197a001a425d7ba
stelcheck <mtrudel@wizcorp.jp>
2018-07-13 12:40:14 +0900

    Release script fixes
    Multiple fixes. Will only release the image or
    the code whenever the version is bumped for either.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -eE
+trap "exit 0" SIGINT
 trap "tailLogs" ERR
 
 function tailLogs() {
@@ -17,7 +18,7 @@ PROJECT_DIR="${SCRIPT_DIR}/../.."
 source "${PROJECT_DIR}/scripts/buildconfig.sh"
 
 binary_name="$(getBuildConfig binary.name)"
-binary_version="$(getBuildConfig binary.version)"
+binary_version="v$(getBuildConfig binary.version)"
 
 image_name="$(getBuildConfig image.organization)/$(getBuildConfig image.name)"
 image_version="$(getBuildConfig image.version)"
@@ -34,43 +35,41 @@ then
 fi
 
 release_args="--user ${github_organization} --repo ${github_name} --tag ${binary_version}"
-GITHUB_RELEASE="docker run -it -e 'GITHUB_TOKEN=${GITHUB_TOKEN}' casualjim/github-release ${release_args}"
+GITHUB_RELEASE="docker run --rm -it -v $(pwd)/src/dist:/dist -e GITHUB_TOKEN=${GITHUB_TOKEN} casualjim/github-release"
 
 if
     ! (git tag | grep -q "^v${binary_version}$")
 then
     echo "* Building binary"
-    ${SCRIPT_DIR}/../build-binary/nix.sh ${binary_version} all ${image_version} >> ./build.log
+    ${SCRIPT_DIR}/../build-binary/nix.sh ${binary_version} all ${image_version} &>> ./build.log
 
     echo "* Pushing v${binary_version} to ${github_repo} (code and tags)"
-    git tag "v${binary_version}"
-    git push --tags git@github.com:${github_repo}.git
+    git tag "${binary_version}" &>> build.log
+    git push --tags git@github.com:${github_repo}.git &>> build.log
 
     echo "* Creating GitHub release ${binary_version}"
-    ${GITHUB_RELEASE} release ${release_args} \
-    --name "${binary_version}" \
-    --description "${description}"
+    ${GITHUB_RELEASE} release ${release_args} --name "${binary_version}" --description "${description}" &>> build.log
 
     echo "* Uploading files to GitHub release"
-    ${GITHUB_RELEASE} upload "${release_args}" --name "${binary_name}-darwin" --file "./src/dist/darwin/${binary_name}"
-    ${GITHUB_RELEASE} upload "${release_args}" --name "${binary_name}-linux" --file "./src/dist/linux/${binary_name}"
-    ${GITHUB_RELEASE} upload "${release_args}" --name "${binary_name}.exe" --file "./src/dist/windows/${binary_name}.exe"
+    ${GITHUB_RELEASE} upload ${release_args} --name "${binary_name}-darwin" --file "/dist/darwin/${binary_name}" &>> build.log
+    ${GITHUB_RELEASE} upload ${release_args} --name "${binary_name}-linux" --file "/dist/linux/${binary_name}" &>> build.log
+    ${GITHUB_RELEASE} upload ${release_args} --name "${binary_name}.exe" --file "/dist/windows/${binary_name}.exe" &>> build.log
 fi
 
 if
     ! docker pull "${image_name}:${image_version}" &> /dev/null
 then
     echo "* Building image"
-    ${SCRIPT_DIR}/../build-image/nix.sh >> ./build.log
+    ${SCRIPT_DIR}/../build-image/nix.sh &>> ./build.log
 
     echo "* Log in to Docker Hub"
     docker login
 
     echo "* Pushing Docker images to Docker Hub (version ${image_version})"
-    docker tag ${image_name} ${image_name}:${image_version}
-    docker tag ${image_name} ${image_name}:latest
-    docker push ${image_name}:${image_version}
-    docker push ${image_name}:latest
+    docker tag ${image_name} ${image_name}:${image_version} &>> build.log
+    docker tag ${image_name} ${image_name}:latest &>> build.log
+    docker push ${image_name}:${image_version} &>> build.log
+    docker push ${image_name}:latest &>> build.log
 fi
 
 echo "* Release ${binary_version} (docker image version ${image_version}) completed"
